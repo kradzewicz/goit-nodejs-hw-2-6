94d3f7a08bdb7677422b5edb74670d294095d77e
Kacper Radzewicz <kacper.radzewicz.26@gmail.com>
2024-12-08 12:41:18 +0100

    first attempt

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require('mongoose')
+require('dotenv').config();
+
+const { DB_HOST: urlDb } = process.env;
+console.log(urlDb)
+const connection = mongoose.connect(urlDb)
+
+
+const startServer = async () => {
+  try {
+    await connection
+    console.log("Database connection successful")
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000")
+    })
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+startServer()
+
